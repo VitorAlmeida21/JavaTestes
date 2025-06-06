e1c3bfb84caf4d218e653bf132467972a27dbfae
VitorAlmeida21 <vitor200970@gmail.com>
2025-06-06 00:08:25 -0300

    Primeiro commit do projeto Java_Exec

---
+++ Semana 1/Q1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class converteTemp{
+    public static void main (String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int C = entrada.nextInt();
+        float F = (C*9/5)+32;
+        System.out.println(C+ " graus Celsius corresponde a " + F + " Fahrenheit.");
+    }
+}
