0ce1694af83af62518e43c70ef50b2bf259a495f
Vitor A. Xavier <81385855+VitorAlmeida21@users.noreply.github.com>
2025-06-25 10:49:13 -0300

    Atualizei a Main para rodar no moodle .|.

--- AulasPOO/src/Semana2/EP3_10.java
+++ AulasPOO/src/Semana2/EP3_10.java
@@ -1,4 +1,5 @@
 package Semana2;
+import java.util.Scanner;
 
 class Calculadora{
     public int somar(int a, int b){
@@ -20,15 +21,35 @@ class Calculadora{
 
 public class EP3_10 {
     public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
         Calculadora calc = new Calculadora();
-
-        System.out.println("Somar dois inteiros " + calc.somar(8, 2));
-
-        System.out.println("Somar três inteiros " + calc.somar(3, 8, 2));
-
-        System.out.println("Somar dois floats " + calc.somar(5.5, 5.5));
-
-        System.out.println("Somar duas strings " + calc.somar("Vi", "tor"));
+        
+        String tipo = s.nextLine();
+        
+        if(tipo.equals("int2")){
+            int x = Integer.parseInt(s.nextLine());
+            int y = Integer.parseInt(s.nextLine());
+            
+            System.out.println(calc.somar(x, y));
+    
+        } else if(tipo.equals("int3")){
+            int x = Integer.parseInt(s.nextLine());
+            int y = Integer.parseInt(s.nextLine());
+            int z = Integer.parseInt(s.nextLine());
+            
+            System.out.println(calc.somar(x, y, z));
+            
+        } else if(tipo.equals("float2")){
+            double x = Double.parseDouble(s.nextLine());
+            double y = Double.parseDouble(s.nextLine());
+            
+            System.out.println(calc.somar(x, y));
+        } else if(tipo.equals("str2")){
+            String x = s.nextLine();
+            String y = s.nextLine();
+            
+            System.out.println(calc.somar(x, y));
+        }
 
     }
 }
