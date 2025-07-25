a53b0a1b8dc27bbb3125b6c745676ea010981e1b
VitorAlmeida21 <vitor200970@gmail.com>
2025-07-25 16:13:13 -0300

    EP7_2

--- AulasPOO/src/Semana6/EP7_2.java
+++ AulasPOO/src/Semana6/EP7_2.java
@@ -1,4 +1,77 @@
 package Semana6;
+import java.util.Scanner;
 
-public class EP7_2 {
+abstract class Veiculo{
+    abstract void acelerar();
 }
+
+class Carro extends Veiculo{
+    public Carro(){
+        System.out.println("Classe Carro criado");
+    }
+
+    @Override
+    public void acelerar(){
+        System.out.println("Carro acelerando suavemente");
+    }
+}
+
+class Moto extends Veiculo{
+    public Moto(){
+        System.out.println("Classe Moto criado");
+    }
+
+    @Override
+    public void acelerar(){
+        System.out.println("Moto acelerando rapidamente");
+    }
+}
+
+class Caminhao extends Veiculo{
+    public Caminhao(){
+        System.out.println("Classe Caminhao criado");
+    }
+
+    @Override
+    public void acelerar(){
+        System.out.println("Caminhao acelerando devagar");
+    }
+}
+
+class FabricaVeiculos{
+    public static Veiculo criar_veiculo(String tipo){
+        switch(tipo.toLowerCase()){
+            case("carro"):
+                return new Carro();
+            case("moto"):
+                return new Moto();
+            case("caminhao"):
+                return new Caminhao();
+            default:
+                System.out.println("Tipo de veículo inválido");
+                return null;
+        }
+    }
+}
+
+class EP7_2{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String tipo = s.nextLine();
+
+        Veiculo veiculo = FabricaVeiculos.criar_veiculo(tipo);
+
+        if(veiculo!=null){
+            veiculo.acelerar();
+        }
+
+        s.close();
+    }
+}
+
+
+
+
+
+
+
